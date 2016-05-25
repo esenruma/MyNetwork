3021e9e72a03a785eaeeac9550e2e50ed190c807
Roma <roma@roma.local>
2016-05-25 15:25:12 +0100

    Labels Set. Now need to add in Stats Code from Contacts Framework Reading.

---
+++ MyNetwork/SettingsViewController.swift
@@ -0,0 +1,60 @@
+//
+//  SettingsViewController.swift
+//  MyNetwork
+//
+//  Created by Roma on 25/05/2016.
+//  Copyright © 2016 esenruma. All rights reserved.
+//
+
+import UIKit
+import Contacts
+
+// ------------------------------------------------------
+let defaultCategories = ["Friends": "F.", "Family": "fam.", "Work": "w.", "Social": "s.", "InnerCircle": "in", "PersonalAdvisor": "ad."]
+
+var firstCat = String()
+var firstCode = String()
+
+var secondCat = String()
+var secondCode = String()
+
+var thirdCat = String()
+var thirdCode = String()
+
+var fourthCat = String()
+var fourthCode = String()
+
+var fifthCat = String()
+var fifthCode = String()
+
+var sixthCat = String()
+var sixthCode = String()
+// ------------------------------------------------------
+
+// ------------------------------------------------------
+
+
+class SettingsViewController: UIViewController {
+
+    
+    
+    
+    
+    
+    
+    
+    
+// ------------------------------------------------------
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
--- MyNetwork/ViewController.swift
+++ MyNetwork/ViewController.swift
@@ -7,12 +7,54 @@
 //
 
 import UIKit
+import Contacts
+
+var categoriesChosen = 0        // 0 = Default, 1 = new Categories
+
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var totalContactsNumber: UILabel!
+    
+    @IBOutlet weak var totalPersonalNetworkNumber: UILabel!
+    
+    @IBOutlet weak var firstCatLabel: UILabel!
+    @IBOutlet weak var firstCatNumber: UILabel!
+    
+    @IBOutlet weak var secondCatLabel: UILabel!
+    @IBOutlet weak var secondCatNumber: UILabel!
+    
+    @IBOutlet weak var thirdCatLabel: UILabel!
+    @IBOutlet weak var thirdCatNumber: UILabel!
+    
+    @IBOutlet weak var fourthCatLabel: UILabel!
+    @IBOutlet weak var fourthCatNumber: UILabel!
+    
+    @IBOutlet weak var fifthCatLabel: UILabel!
+    @IBOutlet weak var fifthCatNumber: UILabel!
+    
+    @IBOutlet weak var sixthCatLabel: UILabel!
+    @IBOutlet weak var sixthCatNumber: UILabel!
+
+    
+// ------------------------------------------------------
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        if categoriesChosen == 0 {
+            self.firstCatLabel.text = "Friends"
+            self.secondCatLabel.text = "Family"
+            self.thirdCatLabel.text = "Work"
+            self.fourthCatLabel.text = "Social"
+            self.fifthCatLabel.text = "Inner Circle"
+            self.sixthCatLabel.text = "Personal Advisor"
+        }
+        
+        
+        
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
